3e69d8352b9c3c6d3e546979a29737c8981b5bfe
Jawad <58373449+Huntsama@users.noreply.github.com>
2023-11-26 17:37:52 +0100

    Update hello-world.c

--- hello-world.c
+++ hello-world.c
@@ -1,69 +1,52 @@
 #include "contiki.h"
 #include "dev/leds.h"
-#include "sys/etimer.h"
-
 #include <stdio.h>
 
-/*---------------------------------------------------------------------------*/
 PROCESS(hello_world_process, "Hello world process");
-PROCESS(led_blink_process, "LED Blink process");
-AUTOSTART_PROCESSES(&hello_world_process, &led_blink_process);
-/*---------------------------------------------------------------------------*/
+AUTOSTART_PROCESSES(&hello_world_process);
+
+static struct etimer hello_timer;
+static struct etimer led_timer;
+static uint8_t counter;
 
 PROCESS_THREAD(hello_world_process, ev, data)
 {
-  static struct etimer timer;
-
   PROCESS_BEGIN();
 
-  /* Setup a periodic timer that expires after 10 seconds. */
-  etimer_set(&timer, CLOCK_SECOND * 10);
+  counter = 0;
+
+  etimer_set(&hello_timer, CLOCK_SECOND * 10);
+  etimer_set(&led_timer, CLOCK_SECOND / 2);
 
   while (1)
   {
-    printf("Hello, world and jawad \n");
+    PROCESS_YIELD();
 
-    /* Wait for the periodic timer to expire and then restart the timer. */
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-    etimer_reset(&timer);
-  }
+    if (ev == PROCESS_EVENT_TIMER && data == &hello_timer)
+    {
+      printf("Hello, world and Jawad\n");
+      etimer_reset(&hello_timer);
+    }
 
-  PROCESS_END();
-}
-
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(led_blink_process, ev, data)
-{
-  static struct etimer led_timer;
-
-  PROCESS_BEGIN();
-
-  /* Setup a periodic timer that expires after 2 seconds for LED blinking. */
-  etimer_set(&led_timer, CLOCK_SECOND * 2);
-
-  while (1)
-  {
-    /* Wait for the LED blinking timer to expire and then restart the timer. */
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&led_timer));
-
-    /* Toggle between red, green, and blue LEDs. */
-    if (leds_get() == LEDS_RED)
+    if (ev == PROCESS_EVENT_TIMER && data == &led_timer)
+    {
+       if (leds_get() == LEDS_RED)
     {
       leds_set(LEDS_GREEN);
     }
     else if (leds_get() == LEDS_GREEN)
     {
-      leds_set(LEDS_BLUE);
+      leds_set(LEDS_YELLOW);
     }
     else
     {
       leds_set(LEDS_RED);
     }
 
-    /* Restart the LED blinking timer. */
-    etimer_reset(&led_timer);
+      counter++;
+      etimer_reset(&led_timer);
+    }
   }
 
   PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
